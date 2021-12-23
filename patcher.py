70c377324abe32f98d74369e4c03189c947ce153
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-12-23 14:53:29 -0500

    Create patcher.py

---
+++ patcher.py
@@ -0,0 +1,14 @@
+fil = input("Select a .PAT file: ")
+file1 = open(fil, 'r')
+Lines = file1.readlines()
+ 
+count = 1
+for line in Lines:
+    if count == 1:
+      myfile = open(fil, 'a')
+    if count == 2:
+      linenum = 
+    if count
+      myfile.writelines('Mage')[1]
+      myfile.close()
+    count += 1
