533e62b0ec9771996d5d5b6fc3675a331875344f
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-12-23 19:18:59 -0500

    Update patcher.py

--- patcher.py
+++ patcher.py
@@ -1,26 +1,27 @@
-fil = input("Select a .PAT file: ")
-file1 = open(fil, 'r')
-Lines = file1.readlines()
+def execute():
+    fil = input("Select a .PAT file: ")
+    file1 = open(fil, 'r')
+    Lines = file1.readlines()
 
-countedarchive = 0
-counted = 0
-count = 1
-for line in Lines:
-    if count == 1:
-      if counted > countedarchive:
-        dator = line
-    if count == 2:
-      if counted > countedarchive:
-        linenum = line
-    if count == 3:
-      if counted > countedarchive:
-        writedata = line
-        countedarchive = counted
-        break
-    count += 1
-    counted += 1
+    countedarchive = 0
+    counted = 0
+    count = 1
+    for line in Lines:
+        if count == 1:
+            if counted > countedarchive:
+                dator = line
+        if count == 2:
+            if counted > countedarchive:
+                linenum = line
+        if count == 3:
+            if counted > countedarchive:
+                writedata = line
+                countedarchive = counted
+                break
+        count += 1
+        counted += 1
 
-file1.close()
-myfile = open(dator, 'a')
-myfile.writelines(writedata)[linenum]
-myfile.close()
+    file1.close()
+    myfile = open(dator, 'a')
+    myfile.writelines(writedata)[linenum]
+    myfile.close()
